dbf3a1822f95fbd5c69dff32cb703d1571dcad81
sergey.aleshin <serega_aleshin@mail.ru>
2022-04-02 19:37:56 +0300

    update

--- habr_parser.py
+++ habr_parser.py
@@ -26,21 +26,33 @@ class HabrParser:
         self.attr_name = attr_name
         self.attr_value = attr_value
 
-    def send_request(self, url=''):
-        if not url:
-            url = self.main_page_url
-        return requests.get(url, headers=self.headers)
+    def get_response(self, post_url=''):
+        url = self.main_page_url if not post_url else post_url
+        try:
+            response = requests.get(url, headers=self.headers)
+            if response.status_code in range(200, 227):
+                return response
+        except Exception as err:
+            print('Вот что я поймал -->', err)
 
-    def get_post_link(self, response):
-        soup = bs4.BeautifulSoup(response.text, 'lxml')
-        all_posts_links = (post.attrs.get('href', '') for post in
-                           soup.find_all(self.tag, {self.attr_name: self.attr_value}))
-        for link in all_posts_links:
-            yield link
+    def get_soup(self, url=''):
+        response = self.get_response(url)
+        try:
+            soup = bs4.BeautifulSoup(response.text, 'lxml')
+            return soup
+        except AttributeError:
+            print('Response == None!')
 
-    def get_post_page_data(self, post_link):
+    def get_post_link(self, soup):
+        if soup:
+            all_posts_links = (post.attrs.get('href', '') for post in
+                               soup.find_all(self.tag, {self.attr_name: self.attr_value}))
+            for link in all_posts_links:
+                yield link
+
+    def get_post_data(self, post_link):
         post_url_full = urljoin(self.main_page_url, post_link)
-        response = self.send_request(post_url_full)
+        response = self.get_response(post_url_full)
         return response
 
     @staticmethod
@@ -50,12 +62,13 @@ class HabrParser:
             file.write(response.content)
 
     def run(self):
-        resp = self.send_request()
-        for link in self.get_post_link(resp):
-            response = self.get_post_page_data(link)
+        soup = self.get_soup()
+        for post_link in self.get_post_link(soup):
+            response = self.get_post_data(post_link)
             self.save_html(response)
 
 
 if __name__ == '__main__':
     parser = HabrParser()
     parser.run()
+    print('finish!')
